4c9d9635a1c21cc3ca2823678d69eac37b8655b9
amit chawla <acamit84@gmail.com>
2020-11-24 01:36:57 +0530

    step 4 authorization to the API to limit the scope

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -36,8 +36,16 @@ namespace Api
                         ValidateAudience = false
                     };
                 });
-            
-                
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiScope", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "api1");
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,7 +64,8 @@ namespace Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllers()
+                .RequireAuthorization("ApiScope");
             });
         }
     }
